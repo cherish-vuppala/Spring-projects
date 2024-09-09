575f40c218de837bae77c0fd40a740e0970fa507
cherish <cherryvuppala@gmail.com>
2024-09-09 07:31:00 +0530

    add lombok annotations

--- src/main/java/com/Cherish/EmployeeManagementSystem/model/Employee.java
+++ src/main/java/com/Cherish/EmployeeManagementSystem/model/Employee.java
@@ -1,10 +1,16 @@
 package com.Cherish.EmployeeManagementSystem.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Employee implements Serializable {
 
         @Id
@@ -18,75 +24,4 @@ public class Employee implements Serializable {
         @Column(nullable = false, updatable = false)
         String employeeCode;
 
-        public Employee() {
-        }
-
-        public Employee(Long id, String name, String jobTitle, String phone, String imageUrl, String employeeCode) {
-                this.id = id;
-                this.name = name;
-                this.jobTitle = jobTitle;
-                this.phone = phone;
-                this.imageUrl = imageUrl;
-                this.employeeCode = employeeCode;
-        }
-
-        public Long getId() {
-                return id;
-        }
-
-        public void setId(Long id) {
-                this.id = id;
-        }
-
-        public String getName() {
-                return name;
-        }
-
-        public void setName(String name) {
-                this.name = name;
-        }
-
-        public String getJobTitle() {
-                return jobTitle;
-        }
-
-        public void setJobTitle(String jobTitle) {
-                this.jobTitle = jobTitle;
-        }
-
-        public String getPhone() {
-                return phone;
-        }
-
-        public void setPhone(String phone) {
-                this.phone = phone;
-        }
-
-        public String getImageUrl() {
-                return imageUrl;
-        }
-
-        public void setImageUrl(String imageUrl) {
-                this.imageUrl = imageUrl;
-        }
-
-        public String getEmployeeCode() {
-                return employeeCode;
-        }
-
-        public void setEmployeeCode(String employeeCode) {
-                this.employeeCode = employeeCode;
-        }
-
-        @Override
-        public String toString() {
-                return "Employee{" +
-                        "id=" + id +
-                        ", name='" + name + '\'' +
-                        ", jobTitle='" + jobTitle + '\'' +
-                        ", phone='" + phone + '\'' +
-                        ", imageUrl='" + imageUrl + '\'' +
-                        ", employeeCode='" + employeeCode + '\'' +
-                        '}';
-        }
 }
